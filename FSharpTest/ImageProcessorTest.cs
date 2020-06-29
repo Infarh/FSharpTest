728fc3f800cd6eb6fa30e06117baea38f8df3292
Infarh <shmachilin@yandex.ru>
2020-06-29 23:07:59 +0300

    wpf...

---
+++ FSharpTest/ImageProcessorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace FSharpTest
+{
+    internal static class ImageProcessorTest
+    {
+        private static void UpdateImage(string url)
+        {
+            Image img = null;
+
+            var client = new WebClient();
+            client.DownloadDataCompleted += (s, e) =>
+            {
+                img = Image.Load(Configuration.Default, e.Result);
+            };
+            client.DownloadDataAsync(new Uri(url));
+
+        }
+    }
+}
---
+++ WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfTest
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoadButtonClick(object Sender, RoutedEventArgs E)
+        {
+            UpdateImage(UrlInput.Text);
+        }
+
+        private void UpdateImage(string url)
+        {
+            var image = img;
+            var client = new WebClient();
+            client.DownloadDataCompleted += (s, e) =>
+            {
+                if (image == null) return;
+                using var stream = new MemoryStream(e.Result);
+                var converter = new ImageSourceConverter();
+                image.Source = (ImageSource)converter.ConvertFrom(stream);
+            };
+            client.DownloadDataAsync(new Uri(url));
+        }
+    }
+}
