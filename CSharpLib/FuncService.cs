1cf1eb2f75313a74c18f556dec0d0d31bef8f630
Infarh <shmachilin@yandex.ru>
2020-06-30 11:14:32 +0300

    Комментарии

--- CSharpLib/FuncService.cs
+++ CSharpLib/FuncService.cs
@@ -6,11 +6,23 @@ namespace CSharpLib
 {
     public static class FuncService
     {
+        /// <summary>Композиция функций</summary>
+        /// <typeparam name="TValue">Тип первичного значения аргумента</typeparam>
+        /// <typeparam name="TArgument">Тип промежуточного значения аргумента</typeparam>
+        /// <typeparam name="TResult">Тип результата</typeparam>
+        /// <param name="GetArgument">Первичная функция, вычисляющая значение аргумента для вторичной функции</param>
+        /// <param name="GetResult">Вторичная функция, вычисляющая итоговое значение</param>
+        /// <returns>Функция композиции двух мункций</returns>
         public static Func<TValue, TResult> Compose<TValue, TArgument, TResult>(
             this Func<TValue, TArgument> GetArgument, 
             Func<TArgument, TResult> GetResult) => 
             value => GetResult(GetArgument(value));
 
+        /// <summary>Потокобезопасная мемоизация с защитой от многократной инициализации значений кеша</summary>
+        /// <typeparam name="T">Тип аргумента функции</typeparam>
+        /// <typeparam name="TResult">Тип значения</typeparam>
+        /// <param name="func">Мемоизируемая функция</param>
+        /// <returns>Функция с мемоизацией</returns>
         public static Func<T, TResult> MemorizeThreadSafeLazy<T, TResult>(this Func<T, TResult> func)
             where T : IComparable
         {
@@ -18,6 +30,11 @@ namespace CSharpLib
             return arg => cache.GetOrAdd(arg, v => new Lazy<TResult>(() => func(v))).Value;
         }
 
+        /// <summary>Потокобезопасная мемоизация</summary>
+        /// <typeparam name="T">Тип аргумента функции</typeparam>
+        /// <typeparam name="TResult">Тип значения</typeparam>
+        /// <param name="func">Мемоизируемая функция</param>
+        /// <returns>Функция с мемоизацией</returns>
         public static Func<T, TResult> MemorizeThreadSafe<T, TResult>(this Func<T, TResult> func)
             where T : IComparable
         {
@@ -25,6 +42,11 @@ namespace CSharpLib
             return arg => cache.GetOrAdd(arg, func);
         }
 
+        /// <summary>Мемоизация функции</summary>
+        /// <typeparam name="T">Тип аргумента функции</typeparam>
+        /// <typeparam name="TResult">Тип значения</typeparam>
+        /// <param name="func">Мемоизируемая функция</param>
+        /// <returns>Функция с мемоизацией</returns>
         public static Func<T, TResult> Memorize<T, TResult>(this Func<T, TResult> func)
             where T : IComparable
         {
