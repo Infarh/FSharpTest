ba756d55ea665ed5cd756c0f0a599364f51fa487
Infarh <shmachilin@yandex.ru>
2020-07-04 19:34:35 +0300

    Использование Atom

---
+++ FSharpTest/Servcies/SignalRStab/ClientWithAtom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using AlgorithmsLib;
+
+namespace FSharpTest.Servcies.SignalRStab
+{
+    abstract class ClientWithAtom : SignalRBase
+    {
+        private readonly Atom<ImmutableDictionary<Guid, string>> _Connections =
+            new Atom<ImmutableDictionary<Guid, string>>(ImmutableDictionary<Guid, string>.Empty);
+
+        public override Task OnConnected()
+        {
+            var connection_id = new Guid(Context.ConnectionId);
+            var user = Context.User;
+            var user_name = user.Identity!.Name;
+
+            var current_conn = _Connections.Value;
+            if (_Connections.Swap(conn => conn.ContainsKey(connection_id) ? conn : conn.Add(connection_id, user_name)) != current_conn)
+                RegisterUserConnection(connection_id, user_name);
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+            var connection_id = new Guid(Context.ConnectionId);
+
+            var current_conn = _Connections.Value;
+            if (current_conn.TryGetValue(connection_id, out var user_name))
+            {
+                DeregisterUserConnection(connection_id, user_name);
+                _Connections.Swap(conn => conn.ContainsKey(connection_id) ? conn.Remove(connection_id) : conn);
+            }
+
+            return base.OnDisconnected();
+        }
+
+
+        protected abstract void RegisterUserConnection(Guid ConnectionId, string UserName);
+
+        protected abstract void DeregisterUserConnection(Guid ConnectionId, string UserName);
+    }
+}
