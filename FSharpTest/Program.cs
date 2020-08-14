f0ceaf252a158098ae62d0621a350430659ee759
Infarh <shmachilin@yandex.ru>
2020-08-14 21:46:06 +0300

    Разнообразные тесты

--- FSharpTest/Program.cs
+++ FSharpTest/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AlgorithmsLib;
 using FSharpLib;
 using FSharpTest.Models;
 using Microsoft.FSharp.Collections;
@@ -9,12 +12,54 @@ namespace FSharpTest
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             //foreach (var i in Enumerable.Empty<int>().AddLast(1,2,3,4,5))
             //    Console.WriteLine($"Value {i}");
 
-            RandomTests.Run();
+            //RandomTests.Run();
+
+            //var items = Enumerable.Range(1, 10).ToArray();
+            //var list = FList.New(items);
+
+            //var items = Enumerable.Range(1, 3).ToArray();
+            //var list = FList<int>.NewRecursive(items);
+
+            var progress = new Progress<(double, double)>(p => Console.Title = $"Progress {p.Item1:p2} : {p.Item2:p2}");
+
+            foreach (var result_task in GetManyDataAsync(10, progress))
+            {
+                var result = await result_task;
+                Console.WriteLine(result);
+            }
+        }
+        
+        private static IEnumerable<Task<double>> GetManyDataAsync(int Count, IProgress<(double, double)> Progress = null, CancellationToken Cancel = default)
+        {
+            Cancel.ThrowIfCancellationRequested();
+            var rnd = new Random();
+            
+            for (var i = 0; i < Count; i++)
+            {
+                var percent = (double) (i + 1) / Count;
+                var task = GetDataAsync(rnd, Progress.Combine((double p) => (percent, p)), Cancel);
+                yield return task;
+                task.Wait(Cancel);
+            }
+        }
+
+        private static async Task<double> GetDataAsync(Random rnd, IProgress<double> Progress, CancellationToken Cancel)
+        {
+            var result = 0d;
+            for (int i = 0, count = 10; i < count; i++)
+            {
+                await Task.Delay(100, Cancel);
+                result += rnd.NextDouble();
+                Cancel.ThrowIfCancellationRequested();
+                Progress?.Report((double)(i+1) / count);
+            }
+
+            return result;
         }
     }
 }
---
+++ FSharpTest/Servcies/ProgressExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+
+namespace System
+{
+    public static class ProgressInfo
+    {
+        private readonly struct SelectProgress<TResult, T> : IProgress<TResult>
+        {
+            private readonly IProgress<T> _Destination;
+            private readonly Func<TResult, T> _Selector;
+
+            public SelectProgress(IProgress<T> Destination, Func<TResult, T> Selector)
+            {
+                _Destination = Destination;
+                _Selector = Selector;
+            }
+
+            public void Report(TResult value) => _Destination?.Report(_Selector(value));
+        }
+
+        public static IProgress<TResult> Combine<T, TResult>(this IProgress<T> Progress, Func<TResult, T> Selector) =>
+            Progress is null
+                ? default
+                : new SelectProgress<TResult, T>(Progress, Selector ?? throw new ArgumentNullException(nameof(Selector)));
+
+        public static IProgress<T> On<T>(Action<T> Reporter) => new Progress<T>(Reporter);
+
+        private class GUIProgress<T> : IProgress<T>
+        {
+            private SynchronizationContext _Context;
+            private readonly Action<T> _Reporter;
+
+            public GUIProgress(Action<T> Reporter)
+            {
+                _Context = new SynchronizationContext();
+                _Reporter = Reporter;
+            }
+
+            public void Report(T value) => throw new NotImplementedException();
+        }
+
+        public static IProgress<T> OnGUI<T>(Action<T> Reporter) => new GUIProgress<T>(Reporter);
+    }
+}
